2edd64f60a3127cdae687d4eeeec8311bf114317
Arslan Mushtaq <arslanark@gmail.com>
2024-06-19 02:25:28 +0400

    feat: extract sections from Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,16 +1,17 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {FlatList, View} from 'react-native';
+import {View} from 'react-native';
+import Button from '../../components/Button';
 import Input from '../../components/Input';
-import Text from '../../components/Text';
 import {DashboardProps} from '../../navigation/DashboardStack/types';
+import DetailSection from '../../sections/DetailSection';
+import IntroductionSection from '../../sections/IntroductionSection';
 import WhoIs, {type NetworkDetails} from '../../services/WhoIs';
 import {styles} from './styles';
-import Button from '../../components/Button';
-import DetailItem from '../../components/DetailItem';
 
 const Dashboard: React.FC<DashboardProps> = (): React.JSX.Element => {
   const [ipInput, setIPInput] = useState('');
   const [ipInputError, setIPInputError] = useState<string | null>(null);
+
   const [details, setDetails] = useState<NetworkDetails[] | null>(null);
 
   const handleIPSearch = useCallback(async () => {
@@ -18,7 +19,6 @@ const Dashboard: React.FC<DashboardProps> = (): React.JSX.Element => {
       if (ipInputError) {
         setIPInputError(null);
       }
-      // Retrieve the IP Address and call the API
       const whois = new WhoIs();
       const result = await whois.getDetails(ipInput);
       setDetails(result);
@@ -29,20 +29,14 @@ const Dashboard: React.FC<DashboardProps> = (): React.JSX.Element => {
   }, [ipInput, ipInputError]);
 
   useEffect(() => {
-    if (details === null) {
+    if (!details) {
       handleIPSearch();
     }
   }, [details, handleIPSearch]);
 
   return (
     <View style={styles.wrapper}>
-      <View style={styles.introWrapper}>
-        <Text variant="heading">IP Tracker</Text>
-        <Text variant="subtitle">
-          Find details from an IP Address. Start by searching for an IP Address
-          below.
-        </Text>
-      </View>
+      <IntroductionSection />
       <View style={styles.inputWrapper}>
         <Input
           keyboardType={'decimal-pad'}
@@ -59,20 +53,7 @@ const Dashboard: React.FC<DashboardProps> = (): React.JSX.Element => {
           SEARCH
         </Button>
       </View>
-      {details && (
-        <View style={styles.detailWrapper}>
-          <FlatList
-            numColumns={2}
-            data={details}
-            columnWrapperStyle={styles.columnStyle}
-            contentContainerStyle={styles.contentStyle}
-            keyExtractor={item => item.title}
-            renderItem={({item}) => {
-              return <DetailItem title={item.title} value={item.value} />;
-            }}
-          />
-        </View>
-      )}
+      <DetailSection details={details} />
     </View>
   );
 };
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -6,22 +6,9 @@ export const styles = StyleSheet.create({
     backgroundColor: 'white',
     rowGap: 2,
   },
-  introWrapper: {
-    margin: 18,
-    rowGap: 8,
-  },
   inputWrapper: {
     padding: 32,
     backgroundColor: 'rgba(211, 208, 203, 0.2)',
     rowGap: 12,
-  },
-  detailWrapper: {
-    rowGap: 8,
-  },
-  columnStyle: {
-    columnGap: 2,
-  },
-  contentStyle: {
-    rowGap: 2,
   },
 });
