6df722583b3954eaf88a98a277573fa288e49e3c
Arslan Mushtaq <arslanark@gmail.com>
2024-06-19 12:02:52 +0400

    feat: display image on detail screen

--- src/screens/Detail/index.tsx
+++ src/screens/Detail/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, ImageSourcePropType, View} from 'react-native';
 import {DetailProps} from '../../navigation/DashboardStack/types';
+import {styles} from './styles';
 
-const Detail: React.FC<DetailProps> = (): React.JSX.Element => {
+const Detail: React.FC<DetailProps> = ({route}): React.JSX.Element => {
+  const {imageId} = route.params;
+
   return (
     <View>
-      <Text>Detail Screen</Text>
+      <Image source={imageId as ImageSourcePropType} style={styles.image} />
     </View>
   );
 };
