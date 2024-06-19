ca5746f3b03011654d3f84cd98058f875fa892a9
Arslan Mushtaq <arslanark@gmail.com>
2024-06-19 18:16:57 +0400

    feat: add market data graph and list and connect to binance stream socket

---
+++ src/screens/MarketData/styles.ts
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignContent: 'center',
+    flex: 1,
+  },
+  graphWrapper: {
+    padding: 16,
+  },
+  graphStyle: {
+    right: '12%',
+  },
+  listWrapper: {
+    flex: 1,
+  },
+  listContentContainerStyle: {
+    padding: 16,
+  },
+  itemWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 24,
+  },
+});
