a4ebcd3526c21652208807e382a2ce4f3456fb71
Arslan Mushtaq <arslanark@gmail.com>
2024-06-19 19:54:44 +0400

    fix: refactor Profile screen to use styles from external stylesheet

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -4,16 +4,14 @@ import {Images} from '../../constants';
 import {ProfileProps} from '../../navigation/ProfileStack/types';
 import {NetworkContext} from '../../providers/NetworkProvider';
 import DetailSection from '../../sections/DetailSection';
+import {styles} from './styles';
 
 const Profile: React.FC<ProfileProps> = (): React.JSX.Element => {
   const {network, image} = useContext(NetworkContext);
 
   return (
-    <View style={{padding: 16, flex: 1, backgroundColor: 'white', rowGap: 12}}>
-      <Image
-        source={image ? image : Images.Gallery[0]}
-        style={{width: '100%', height: '50%', borderRadius: 12}}
-      />
+    <View style={styles.wrapper}>
+      <Image source={image ? image : Images.Gallery[0]} style={styles.image} />
       <DetailSection details={network} />
     </View>
   );
