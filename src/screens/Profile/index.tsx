0f82d556efe3a93f3895044892ebc936a2069a52
Arslan Mushtaq <arslanark@gmail.com>
2024-06-19 19:30:51 +0400

    feat: add profile screen components

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {Image, View} from 'react-native';
+import {Images} from '../../constants';
 import {ProfileProps} from '../../navigation/ProfileStack/types';
+import {NetworkContext} from '../../providers/NetworkProvider';
+import DetailSection from '../../sections/DetailSection';
 
 const Profile: React.FC<ProfileProps> = (): React.JSX.Element => {
+  const {network} = useContext(NetworkContext);
+
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View style={{padding: 16, flex: 1, backgroundColor: 'white', rowGap: 12}}>
+      <Image
+        source={Images.Gallery[0]}
+        style={{width: '100%', height: '50%', borderRadius: 12}}
+      />
+      <DetailSection details={network} />
     </View>
   );
 };
